e91ea919426555b14da3171fc779edf1b868671c
Dominic-Talgatto <110767895+Dominic-Talgatto@users.noreply.github.com>
2024-02-21 21:34:24 +0600

    Add files via upload

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,99 @@
+export interface Product {
+  img: string;
+  name: string;
+  description: string;
+  rating: number;
+  link: string;
+}
+
+export const products = [
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h1c/hf1/85128198684702.png?format=gallery-large',
+    name: 'Coca-Cola Classic газированный напиток 2 л',
+    description:
+      'Coca-Cola – один из самых популярных безалкогольных напитков в истории, а также один из наиболее узнаваемых брендов в мире.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/coca-cola-classic-gazirovannyi-napitok-2-l-100208094/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h21/h90/70365637279774.jpg?format=gallery-medium',
+    name: 'Рахат шоколадная плитка молочный 100 г',
+    description:
+      'Особенно вкусный молочный шоколад Казахстанский с ванильным ароматом изготовлен из отборных какао-бобов по классической технологии на шоколадной фабрике Рахат. Натуральный шоколад зарядит Вас энергией и подарит радость на весь день.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/rahat-shokoladnaja-plitka-molochnyi-100-g-100221859/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h93/hde/63820990414878.jpg?format=gallery-medium',
+    name: 'Алекс минеральная Saryagash газированная 1.5 л',
+    description:
+      'Минеральная вода Алекс Сары Агаш не только утолит жажду, но и наполнит каждую клеточку Вашего организма витаминами.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/aleks-mineral-naja-saryagash-gazirovannaja-1-5-l-100211003/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/hf5/hcd/85049851936798.jpg?format=gallery-medium',
+    name: 'MAGNUM яблоко Голден Казахстан',
+    description:
+      'Полное название сорта звучит как Золотое превосходное или Голден делишес. По статистики яблоки сорта голден относят к самым востребованным сортом среди потребителей свежих яблок. Под этот сорт отведены большие площади в США. Происхождение золотых яблок до конца неизвестно.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/magnum-jabloko-golden-kazahstan-101639291/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h5c/hdc/65063405879326.jpg?format=gallery-medium',
+    name: 'Яйцо ALSAD куриное 30 шт 60-65 г',
+    description: 'TOP.',
+    rating: 4.5,
+    link: 'https://kaspi.kz/shop/p/alsad-kurinoe-30-sht-60-65-g-105515318/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h4e/hef/84878659846174.jpg?format=gallery-medium',
+    name: 'MAGNUM мандарин Турция',
+    description: 'TOP.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/magnum-mandarin-turtsija-104342837/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h16/h8d/64322043871262.jpg?format=gallery-medium',
+    name: 'MAGNUM помидор на ветке',
+    description: 'TOP.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/magnum-pomidor-na-vetke-101672554/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h31/h52/64520208023582.jpg?format=gallery-medium',
+    name: 'MAGNUM киви Чили 0.5 кг',
+    description: 'TOP.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/magnum-kivi-chili-0-5-kg-105822254/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h12/hc9/63786315874334.jpg?format=gallery-medium',
+    name: 'Майонез 3 желания провансаль 67% 380 г',
+    description: 'TOP.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/maionez-3-zhelanija-provansal-67-380-g-100224019/?c=750000000',
+  },
+
+  {
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h3e/h77/84968157184030.jpg?format=gallery-medium',
+    name: 'Baron вареники картофель 400 г',
+    description: 'TOP.',
+    rating: 5,
+    link: 'https://kaspi.kz/shop/p/baron-vareniki-kartofel-400-g-101584866/?c=750000000',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
